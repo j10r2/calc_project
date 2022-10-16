7792ea7b56768ecc31c08ba22bb8f1bcfcbca8d6
natalya <lazovskaya77@mail.ru>
2022-10-16 15:04:05 +0500

    made corrections

--- calc_complex.py
+++ calc_complex.py
@@ -1,23 +1,22 @@
-x1 = int(input("Введите действительную часть числа: "))
-y1 = int(input("Введите мнимую часть числа: "))
+
+
+
+
+
+x1 = int(input("Введите действительную часть числа 1: "))
+y1 = int(input("Введите мнимую часть числа 1 : "))
 
 num1 = complex(x1, y1)
 
-x2 = int(input("Введите действительную часть числа: "))
-y2 = int(input("Введите мнимую часть числа: "))
+x2 = int(input("Введите действительную часть числа 2: "))
+y2 = int(input("Введите мнимую часть числа 2: "))
 
 num2 = complex(x2, y2)
 print(num1)
 print(num2)
 
-def complex():
-    if operand == 1:
-        return num1 + num2
-    elif operand == 2:
-        return num1 - num2
-    elif operand == 3:
-        return num1 * num2
-    elif operand == 4:
-        return num1 / num2
+print('сумма  =', num1 + num2)
+print('разность =', num1 - num2)
+print('произведение =', num1 * num2)
+print('частное =', num1 / num2)
 
-print(res)
